cf47342afb8813d4197d6186b163722bbefce04d
pontused <pontus.edstrom@gmail.com>
2015-04-22 10:54:11 +0200

    Locking playlist

--- Client/SadokSync/src/main/java/com/sadoksync/sadoksync/PublicPlaylist.java
+++ Client/SadokSync/src/main/java/com/sadoksync/sadoksync/PublicPlaylist.java
@@ -1,43 +1,114 @@
 package com.sadoksync.sadoksync;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  *
  * @author Arouz
  */
-public class PublicPlaylist {
+public class PublicPlaylist implements Serializable  {
+
+    final Lock lock;
+    final Condition ocupied;
 
     ArrayList<Pair> playlist;
 
     public PublicPlaylist() {
         playlist = new ArrayList<Pair>();
+        lock = new ReentrantLock();
+        ocupied = lock.newCondition();
     }
 
+    /*
+     Returns the Lock used by the playlist
+     */
+    public Lock getLock() {
+        return lock;
+    }
+
+    /*
+     Return the condition variable used by the playlist
+     */
+    public Condition getCV() {
+        return ocupied;
+    }
+
+    /*
+     Critical section locked with lock.
+     Adds an item to the playlists.
+     */
     public void addToPlaylist(String name, String path, String length, String type) {
-        playlist.add(new Pair("Sadok", new Media(name, path, length, type)));
+        lock.lock();
+        try {
+            playlist.add(new Pair("Sadok", new Media(name, path, length, type)));
+            ocupied.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
     }
 
+    /*
+     Returnerar playlisten. Är inte syncronizerad. 
+     Ett lock måste sättas kring denna användning.
+     //locket måste sitta runt det som kallar getMediaList...
+     */
     public ArrayList<Pair> getMediaList() {
         return playlist;
     }
 
+    /*
+     lås och läs vad som spelas nu.
+     */
     public String getNowPlaying() {
-        Pair pair = playlist.get(0);
-       
-        Media Value = pair.value();
+        Pair pair;
+        Media Value;
+        lock.lock();
+        try {
+            pair = playlist.get(0);
+            Value = pair.value();
+
+            ocupied.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
         return Value.getPath();
     }
-    
+
+    /*
+     Lock the plalist and remove the first pair
+     */
     public void removeFirstInQueue() {
-        playlist.remove(0);
+        lock.lock();
+        try {
+            playlist.remove(0);
+
+            ocupied.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     public boolean isEmpty() {
-        return playlist.isEmpty();
+        boolean ret;
+        lock.lock();
+        try {
+            ret = playlist.isEmpty();
+
+            ocupied.signalAll();
+        } finally {
+            lock.unlock();
+        }
+        return ret;
     }
 
-    public static class Pair {
+    public static class Pair implements Serializable {
 
         private final String key;
         private final Media value;
