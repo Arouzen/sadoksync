93988a436fa3b3947d462ffdecf17a6852e44b0a
Arouzen <arouzen@gmail.com>
2015-04-24 14:44:37 +0200

    FileFilter now uses json

--- Client/SadokSync/src/main/java/com/sadoksync/sadoksync/FileFilter.java
+++ Client/SadokSync/src/main/java/com/sadoksync/sadoksync/FileFilter.java
@@ -7,6 +7,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -16,33 +20,36 @@ class FileFilter extends javax.swing.filechooser.FileFilter {
 
     @Override
     public boolean accept(File file) {
-        FileReader fileReader = null;
         boolean allowed = false;
+        JSONParser JSONParser = new JSONParser();
+
         try {
             // Allow only extentions within the AllowedVLCExtensions.txt
             StringBuilder location = new StringBuilder(Client.class.getProtectionDomain().getCodeSource().getLocation().toString());
             location.delete(0, 6);
-            File f = new File(location.toString() + "/AllowedVLCExtensions.txt");
-            fileReader = new FileReader(f);
-            BufferedReader br = new BufferedReader(fileReader);
-            String line = null;
-            // if no more lines the readLine() returns null
-            while ((line = br.readLine()) != null) {
-                if (file.getAbsolutePath().endsWith("." + line) || file.isDirectory()) {
+            File f = new File(location.toString() + "/VLCExtensions.json");
+            JSONObject jsonObject = (JSONObject) JSONParser.parse(new FileReader(f));
+            JSONObject jsonMediaObject = (JSONObject) jsonObject.get("media");
+            JSONArray jsonVisualizeArray = (JSONArray) jsonMediaObject.get("visualize");
+            JSONArray jsonVideoArray = (JSONArray) jsonMediaObject.get("video");
+
+            for (Object type : jsonVisualizeArray) {
+                if (file.getAbsolutePath().endsWith("." + type) || file.isDirectory()) {
                     allowed = true;
                     break;
                 }
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println("AllowedVLCExtensions.txt file not found");
-        } catch (IOException ex) {
-            System.out.println("AllowedVLCExtensions.txt file interrupted");
-        } finally {
-            try {
-                fileReader.close();
-            } catch (IOException ex) {
-                Logger.getLogger(FileFilter.class.getName()).log(Level.SEVERE, null, ex);
+            if (!allowed) {
+                for (Object type : jsonVideoArray) {
+                    if (file.getAbsolutePath().endsWith("." + type) || file.isDirectory()) {
+                        allowed = true;
+                        break;
+                    }
+                }
             }
+
+        } catch (IOException | ParseException ex) {
+            Logger.getLogger(FileFilter.class.getName()).log(Level.SEVERE, null, ex);
         }
         return allowed;
     }
